e3658cb012d78d5466fc48494ddff2e13ca2e595
Huy Huy <nhathuych@gmail.com>
2025-03-13 21:31:58 +0700

    add validation pip & dto

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get('PORT') ?? 3001;
+
+  app.setGlobalPrefix('api/v1', { exclude: [''] });
+
+  // Pipe là một công cụ dùng để xử lý dữ liệu trước khi dữ liệu được truyền vào controller.
+  // Pipe có thể làm các công việc như:
+  // Kiểm tra dữ liệu đầu vào (Validation)
+  // Chuyển đổi dữ liệu (Transform)
+  // Loại bỏ các dữ liệu không hợp lệ (Filtering)
+  // Trường hợp này ValidationPipe tự validate giùm các trường trong dto
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true, // loại bỏ các field không được định nghĩa trong DTO
+    forbidNonWhitelisted: true, // Báo lỗi nếu có field lạ
+    transform: true, // Tự động convert kiểu dữ liệu
+  }));
 
   await app.listen(port);
 }
--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,1 +1,15 @@
-export class CreateUserDto {}
+// DTO:Data Transfer Object
+// Là một class dùng để định nghĩa cấu trúc dữ liệu mà client gởi lên server.
+
+import { IsNotEmpty } from "class-validator";
+
+// Ko gởi thừa/thiếu vì 1 số trường mặt định tự tạo như isActive(default: false), createdAt, updatedAt,...
+export class CreateUserDto {
+  @IsNotEmpty()
+  name: string;
+  // email: string;
+  // password: string;
+  // phone: string;
+  // address: string;
+  // image: string;
+}
