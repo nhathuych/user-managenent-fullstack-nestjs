c334b05d9d53edb920029804dbabcc8e481cfb75
Huy Huy <nhathuych@gmail.com>
2025-03-14 21:44:15 +0700

    check create user api & add validation

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,15 +1,26 @@
 // DTO:Data Transfer Object
 // Là một class dùng để định nghĩa cấu trúc dữ liệu mà client gởi lên server.
 
-import { IsNotEmpty } from "class-validator";
+import { IsEmail, IsNotEmpty, IsOptional } from "class-validator";
 
 // Ko gởi thừa/thiếu vì 1 số trường mặt định tự tạo như isActive(default: false), createdAt, updatedAt,...
 export class CreateUserDto {
   @IsNotEmpty()
   name: string;
-  // email: string;
-  // password: string;
-  // phone: string;
-  // address: string;
-  // image: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  password: string;
+
+  @IsOptional()
+  phone: string;
+
+  @IsOptional()
+  address: string;
+
+  @IsOptional()
+  image: string;
 }
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,13 +1,33 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './schemas/user.schema';
+import { Model } from 'mongoose';
 
 // @Injectable() giúp tạo 1 singleton
 // Tự nestjs sẽ tạo đối tượng, ta không cần phải làm như vầy: new UsersService()
 // Chỉ cần làm như vầy: private userService: UserService
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {
+    // @InjectModel(User.name): inject schema User vào trong Service để lấy schema User từ MongoDB
+
+    // private userModel: chỉ dùng userModel trong class UsersService này
+    // Tiết kiệm dòng code, không cần phải làm như vầy:
+    // private userModel: Model<User>; // tương tự cho public và protected
+    // constructor(@InjectModel(User.name) userModel: Model<User>) {
+    //   this.userModel = userModel;
+    // }
+
+    // Model<User>
+    // Định nghĩa kiểu dữ liệu là User cho Model của mongo
+    // Không có <User> => await this.userModel.find(); // Kiểu trả về là any[]
+    // Có <User>       => await this.userModel.find(); // Kiểu trả về là User[]
+  }
+
+  async create(createUserDto: CreateUserDto) {
+    console.log(createUserDto)
     return 'This action adds a new user';
   }
 
